6901a56e43dcc73324f2b69755802cfa404228cd
tahsin-soufizadeh <tahsinsoufizade@gmail.com>
2021-09-12 10:21:20 +0430

    add all project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const dragItems = document.querySelectorAll('.drag-item');
+const dragWrapper = document.querySelectorAll('.drag-wrapper');
+
+
+dragItems.forEach(item => {
+    item.addEventListener('dragstart', () => {
+        item.classList.add('dragging');
+    });
+
+    item.addEventListener('dragend', () => {
+        item.classList.remove('dragging');
+    });
+});
+
+
+dragWrapper.forEach(parent => {
+    parent.addEventListener('dragover', (e) => {
+        const draggable = document.querySelector('.dragging');
+        const afterElement = getDragAfterElement(parent, e.clientX);
+
+        if (afterElement == null) {
+            parent.append(draggable);
+        } else {
+            parent.insertBefore(draggable, afterElement);
+        }
+    });
+});
+
+function getDragAfterElement(parent, y) {
+    const draggableElements = [...parent.querySelectorAll('.drag-item:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.left - box.height / 2;
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
